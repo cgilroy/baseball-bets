920d1d6c1bdbd6b1d77a45dac07140e9182fad42
CJ Gilroy <c.gilroy9@gmail.com>
2019-05-08 10:08:11 -0600

    Organizing data from top level parent component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FetchData from './FetchData.js'
+import MatchupBlock from './MatchupBlock.js'
 import {useState, useEffect} from 'react'
 
 function App() {
-  const [gamesData, setGamesData] = useState([])
+  const [allData, setAllData] = useState()
+
   useEffect(() => {
-    FetchData()
+    FetchData(setAllData)
   },[])
+  console.log(allData,'allData')
+  let matchupBlocks
+  if (allData !== undefined) {
+    let schedule = allData.find(obj => obj.dataType === "schedule")
+    let pitchingData = allData.find(obj => obj.dataType === "pitching").data
+    let hittingData = allData.find(obj => obj.dataType === "hitting").data
+    let fieldingData = allData.find(obj => obj.dataType === "fielding").data
+
+    matchupBlocks = schedule.data.games.map(game => {
+      let homeData = {
+        id: game.teams.home.team.id,
+        name: game.teams.home.team.name,
+        record: game.teams.home.leagueRecord,
+        pitching: pitchingData.find(obj => obj.team.id === game.teams.home.team.id).stat,
+        hitting: hittingData.find(obj => obj.team.id === game.teams.home.team.id).stat,
+        fielding: fieldingData.find(obj => obj.team.id === game.teams.home.team.id).stat
+      }
+      let awayData = {
+        id: game.teams.away.team.id,
+        name: game.teams.away.team.name,
+        record: game.teams.away.leagueRecord,
+        pitching: pitchingData.find(obj => obj.team.id === game.teams.away.team.id).stat,
+        hitting: hittingData.find(obj => obj.team.id === game.teams.away.team.id).stat,
+        fielding: fieldingData.find(obj => obj.team.id === game.teams.away.team.id).stat
+      }
+      return <MatchupBlock gameData={game} homeData={homeData} awayData={awayData} />
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>baseball-bets</h1>
+
       </header>
-      {gamesData}
+      {matchupBlocks}
     </div>
   );
 }
