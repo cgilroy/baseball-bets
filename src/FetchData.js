63bb3ae0fd58226f0969e52609971ac619c421b1
CJ Gilroy <c.gilroy9@gmail.com>
2019-05-08 08:38:30 -0600

    Changing how data fetch is done

--- src/FetchData.js
+++ src/FetchData.js
@@ -7,27 +7,22 @@ function FetchData(callback) {
   let fieldingData = ''
 
   var fetches = []
+  const urls = [
+    'http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1',
+    'https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=pitching&sportIds=1',
+    'https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=hitting&sportIds=1',
+    'https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=fielding&sportIds=1'
+  ]
 
-  fetches.push(
-    scheduleData = fetch('http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1')
-    .then(results => results.json())
-  )
-  fetches.push(
-    fetch('https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=pitching&sportIds=1')
-    .then(results => results.json()).then()
-  )
-  fetches.push(
-    hittingData = fetch('https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=hitting&sportIds=1')
-    .then(results => results.json())
-  )
-  fetches.push(
-    fieldingData = fetch('https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=fielding&sportIds=1')
-    .then(results => results.json())
-  )
-
-  Promise.all(fetches).then(() => {
-    console.log([scheduleData,pitchingData,hittingData,fieldingData],'test')
-  })
+  Promise.all(urls.map(url =>
+    fetch(url).then(resp => resp.json()).then(data => {
+      if (data.stats !== undefined) {
+        return {dataType: data.stats['0'].group.displayName,data:data.stats['0'].splits}
+      } else if (data.dates !== undefined) {
+        return {dataType:'schedule',data: data.dates['0']}
+      }
+    })
+  )).then(dataObj => callback(dataObj))
 }
 
 export default FetchData
