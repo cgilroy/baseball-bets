0d2e7cf173931ee2cd1c0cb2231832fd7c55a957
CJ Gilroy <c.gilroy9@gmail.com>
2019-07-11 13:33:39 -0600

    Split backend fetch to team stats and pitching

--- backend/FetchData.js
+++ backend/FetchData.js
@@ -28,16 +28,57 @@ function FetchTeamData(date,callback) {
     )).then(dataObj => {
       let scheduleData = dataObj.find(obj => obj.dataType === "schedule")
       if (scheduleData.data !== undefined) {
+        fetch('https://statsapi.mlb.com/api/v1/standings?leagueId=103,104').then(resp => resp.json()).then(output => {
+          // console.log(output,'output')
+          let splitRecords = []
+          output.records.map(divisions => {
+            divisions.teamRecords.map(teams => {
+              splitRecords.push(
+                {
+                  id: teams.team.id,
+                  wins: teams.leagueRecord.wins,
+                  losses: teams.leagueRecord.losses,
+                  totPct: teams.leagueRecord.pct,
+                  dayPct: teams.records.splitRecords.find(obj => obj.type === 'day').pct,
+                  nightPct: teams.records.splitRecords.find(obj => obj.type === 'night').pct,
+                  lastTen: teams.records.splitRecords.find(obj => obj.type === 'lastTen').pct,
+                  home: teams.records.splitRecords.find(obj => obj.type === 'home').pct,
+                  away: teams.records.splitRecords.find(obj => obj.type === 'away').pct
+                }
+              )
+            })
+          })
+          // console.log(nextData,'nextData')
+          dataObj.push({dataType: 'records',data: splitRecords})
+          callback(dataObj.filter(obj => (obj.dataType !== 'schedule' && obj.dataType !== 'startingPitcherStats')))
+        })
+      } else {
+        // no games scheduled, return nothing
+        callback()
+      }
+    })
+  }
+
+  function FetchStartingPitcherStats(date,storedData,callback) {
+    let dateUrl = 'https://statsapi.mlb.com/api/v1/schedule?date=' + date + '&sportId=1&hydrate=probablePitcher(note)'
+    var outputData = storedData.pitcherData !== undefined ? storedData.pitcherData.data : [];
+    // console.log(storedData,'inital')
+    fetch(dateUrl).then(resp => resp.json()).then(data => {
+      if (data.dates !== undefined) {
+        let scheduleData = data.dates['0']
+
         let pitcherUrls = []
-        scheduleData.data.games.map(game => {
-          if (game.teams.home.probablePitcher && game.teams.away.probablePitcher) {
+        scheduleData.games.map(game => {
+          // only fetch new pitcher data if game is not underway
+          // we want to store historical pre-game pitcher data in database
+          if (game.status.codedGameState === 'S' && game.teams.home.probablePitcher && game.teams.away.probablePitcher) {
             pitcherUrls.push(
               `https://statsapi.mlb.com/api/v1/people/${game.teams.home.probablePitcher.id}/stats?stats=byDateRange&season=2019&group=pitching`,
               `https://statsapi.mlb.com/api/v1/people/${game.teams.away.probablePitcher.id}/stats?stats=byDateRange&season=2019&group=pitching`
             )
           }
         })
-        let allPitcherStats = []
+        // console.log('pitcherUrls',pitcherUrls)
         Promise.all(pitcherUrls.map(url =>
           fetch(url).then(resp =>
             resp.json().then(parsed => {
@@ -48,44 +89,28 @@ function FetchTeamData(date,callback) {
                 }
               )
             })
-          ).then(data => allPitcherStats.push(data))
+          ).then(data => {
+            // console.log('findingIndex',outputData)
+            let foundIndex = outputData.findIndex(obj => obj.id === data.id)
+            foundIndex !== -1 ? outputData[foundIndex] = data : outputData.push(data);
+          })
           .catch(error => console.log(error))
         )).then(() => {
-          dataObj.push({dataType:"startingPitcherStats",data:allPitcherStats});
-          return dataObj
-        }).then(nextData => {
-          fetch('https://statsapi.mlb.com/api/v1/standings?leagueId=103,104').then(resp => resp.json()).then(output => {
-            // console.log(output,'output')
-            let splitRecords = []
-            output.records.map(divisions => {
-              divisions.teamRecords.map(teams => {
-                splitRecords.push(
-                  {
-                    id: teams.team.id,
-                    wins: teams.leagueRecord.wins,
-                    losses: teams.leagueRecord.losses,
-                    totPct: teams.leagueRecord.pct,
-                    dayPct: teams.records.splitRecords.find(obj => obj.type === 'day').pct,
-                    nightPct: teams.records.splitRecords.find(obj => obj.type === 'night').pct,
-                    lastTen: teams.records.splitRecords.find(obj => obj.type === 'lastTen').pct,
-                    home: teams.records.splitRecords.find(obj => obj.type === 'home').pct,
-                    away: teams.records.splitRecords.find(obj => obj.type === 'away').pct
-                  }
-                )
-              })
-            })
-            // console.log(nextData,'nextData')
-            nextData.push({dataType: 'records',data: splitRecords})
-            callback(nextData.filter(obj => (obj.dataType !== 'schedule' && obj.dataType !== 'startingPitcherStats')))
-          })
+          // console.log('callingBack',outputData);
+          if (storedData.pitcherData !== undefined) {
+            (outputData !== undefined && outputData !== storedData.pitcherData.data) ? callback(outputData) : callback([]);
+          } else {
+            (outputData !== undefined) ? callback(outputData) : callback([]);
+          }
         })
       } else {
-        // no games scheduled, return nothing
-        callback()
+        callback([])
       }
     })
+
   }
 
   module.exports = {
-    fetchTeamData: FetchTeamData
+    fetchTeamData: FetchTeamData,
+    fetchStartingPitcherStats: FetchStartingPitcherStats
   }
