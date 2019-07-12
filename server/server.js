8a178237c1ec695cc2cdbad68da0a92f4443bc60
CJ Gilroy <c.gilroy9@gmail.com>
2019-07-12 14:21:41 -0600

    Try removing react default routing

--- server/server.js
+++ server/server.js
@@ -71,12 +71,9 @@ server.listen(port, () => {
                 response.send(result.result);
             });
         });
-        server.use(Express.static('../build'));
             
         // Handle React routing, return all requests to React app
-        server.get('*', function(req, res) {
-          res.sendFile(path.join('../build', 'index.html'));
-        });
+
 
         // if (process.env.NODE_ENV === 'production') {
         //   // Serve any static files
