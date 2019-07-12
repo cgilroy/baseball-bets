abb5cb95a9f97599e373d7fd693d7578b6a9d572
CJ Gilroy <c.gilroy9@gmail.com>
2019-07-12 14:03:25 -0600

    Remove /client from path

--- server/server.js
+++ server/server.js
@@ -74,11 +74,11 @@ server.listen(port, () => {
 
         if (process.env.NODE_ENV === 'production') {
           // Serve any static files
-          server.use(express.static(path.join(__dirname, 'client/build')));
+          server.use(express.static(path.join(__dirname, '/build')));
             
           // Handle React routing, return all requests to React app
           server.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+            res.sendFile(path.join(__dirname, '/build', 'index.html'));
           });
         }
 
