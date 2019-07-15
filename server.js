012347fc06e7ddd60c8d68d0e63231aebf5e3a32
CJ Gilroy <c.gilroy9@gmail.com>
2019-07-15 09:35:23 -0600

    Remove /static from .use expression

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use(BodyParser.urlencoded({ extended: true }));
 
 var database, collection;
 
-server.use('/static',Express.static(path.join(__dirname, '/build')));
+server.use(Express.static(path.join(__dirname, '/build')));
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
