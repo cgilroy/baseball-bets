b51f8ca9b85dfb2d5c560deb517d277de4e359b5
cgilroy <c.gilroy9@gmail.com>
2022-05-09 12:01:12 -0700

    Make data fetching work for current year

--- backend/FetchData.js
+++ backend/FetchData.js
@@ -1,16 +1,15 @@
 const fetch = require("node-fetch")
 
 function FetchTeamData(date,callback) {
-    // console.log(date,'date')
     var testingLiveData = false
     let dateUrl = 'https://statsapi.mlb.com/api/v1/schedule?date='+date+'&sportId=1&hydrate=probablePitcher(note)'
-    // let dateUrl = 'https://statsapi.mlb.com/api/v1/schedule?date=12/28/2019&sportId=1&hydrate=probablePitcher(note)'
-    // console.log(dateUrl,'dateUrl')
+
+    let year = date.split("-")[0]
     const urls = [
       dateUrl,
-      'https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=pitching&sportIds=1',
-      'https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=hitting&sportIds=1',
-      'https://statsapi.mlb.com/api/v1/teams/stats?season=2019&stats=season&group=fielding&sportIds=1'
+      `https://statsapi.mlb.com/api/v1/teams/stats?season=${year}&stats=season&group=pitching&sportIds=1`,
+      `https://statsapi.mlb.com/api/v1/teams/stats?season=${year}&stats=season&group=hitting&sportIds=1`,
+      `https://statsapi.mlb.com/api/v1/teams/stats?season=${year}&stats=season&group=fielding&sportIds=1`
     ]
   
     Promise.all(urls.map(url =>
@@ -29,7 +28,6 @@ function FetchTeamData(date,callback) {
       let scheduleData = dataObj.find(obj => obj.dataType === "schedule")
       if (scheduleData.data !== undefined) {
         fetch('https://statsapi.mlb.com/api/v1/standings?leagueId=103,104').then(resp => resp.json()).then(output => {
-          // console.log(output,'output')
           let splitRecords = []
           output.records.map(divisions => {
             divisions.teamRecords.map(teams => {
@@ -48,7 +46,6 @@ function FetchTeamData(date,callback) {
               )
             })
           })
-          // console.log(nextData,'nextData')
           dataObj.push({dataType: 'records',data: splitRecords})
           callback(dataObj.filter(obj => (obj.dataType !== 'schedule' && obj.dataType !== 'startingPitcherStats')))
         })
@@ -62,25 +59,23 @@ function FetchTeamData(date,callback) {
   function FetchStartingPitcherStats(date,storedData,callback) {
     let dateUrl = 'https://statsapi.mlb.com/api/v1/schedule?date=' + date + '&sportId=1&hydrate=probablePitcher(note)'
     let outputData = storedData.pitcherData !== undefined ? [  ...storedData.pitcherData.data ] : [];
-    // console.log(outputData,'initialoutput')
-    // console.log(storedData.pitcherData.data,'inital')
-    // console.log(typeof(storedData.pitcherData.data),'inital')
+
     fetch(dateUrl).then(resp => resp.json()).then(data => {
       if (data.dates !== undefined) {
         let scheduleData = data.dates['0']
 
         let pitcherUrls = []
+        let year = new Date().getFullYear()
         scheduleData.games.map(game => {
           // only fetch new pitcher data if game is not underway
           // we want to store historical pre-game pitcher data in database
           if ((game.status.codedGameState === 'S' || game.status.codedGameState === 'P') && game.teams.home.probablePitcher && game.teams.away.probablePitcher) {
             pitcherUrls.push(
-              `https://statsapi.mlb.com/api/v1/people/${game.teams.home.probablePitcher.id}/stats?stats=byDateRange&season=2019&group=pitching`,
-              `https://statsapi.mlb.com/api/v1/people/${game.teams.away.probablePitcher.id}/stats?stats=byDateRange&season=2019&group=pitching`
+              `https://statsapi.mlb.com/api/v1/people/${game.teams.home.probablePitcher.id}/stats?stats=yearByYear&season=${year}&group=pitching`,
+              `https://statsapi.mlb.com/api/v1/people/${game.teams.away.probablePitcher.id}/stats?stats=yearByYear&season=${year}&group=pitching`
             )
           }
         })
-        // console.log('pitcherUrls',pitcherUrls.length)
         Promise.all(pitcherUrls.map(url =>
           fetch(url).then(resp =>
             resp.json().then(parsed => {
@@ -89,7 +84,7 @@ function FetchTeamData(date,callback) {
               try {
                 pitcherStats = {
                   id: pitchID,
-                  stats: parsed.stats['0'].splits['0'].stat
+                  stats: parsed.stats['0'].splits.find(seasonStats => seasonStats.season == year).stat
                 }
               } catch {
                 pitcherStats = {
@@ -102,24 +97,15 @@ function FetchTeamData(date,callback) {
               return pitcherStats
             })
           ).then(data => {
-            // console.log('findingIndex',data)
-            if (typeof(outputData) === 'object') {
-              // console.log('differentData')
-              // console.log(outputData,'addingsome')
-              
+            if (typeof(outputData) === 'object') {              
               let foundIndex = outputData.findIndex(obj => {
-                // console.log(obj.id,'obj id')
-                // console.log(data.id,'data id')
                 return obj.id === data.id 
               })
-              // console.log(foundIndex,'foundIndex')
               foundIndex !== -1 ? outputData[foundIndex] = data : outputData.push(data);
             }
           })
           .catch(error => console.log(error))
         )).then(() => {
-          // console.log(Object.keys(outputData),'outputData');
-          // console.log(Object.keys(storedData.pitcherData.data),'storedData')
           if (storedData.pitcherData !== undefined) {
             (outputData !== undefined && !isEquivalent(outputData,storedData.pitcherData.data)) ? callback(outputData) : callback([]);
           } else {
@@ -137,8 +123,6 @@ function FetchTeamData(date,callback) {
     // Create arrays of property names
     var aProps = Object.getOwnPropertyNames(a);
     var bProps = Object.getOwnPropertyNames(b);
-    // console.log(aProps,'ap')
-    // console.log(bProps, 'bp')
 
     // If number of properties is different,
     // objects are not equivalent
@@ -148,7 +132,6 @@ function FetchTeamData(date,callback) {
 
     for (var i = 0; i < aProps.length; i++) {
         var propName = aProps[i];
-        // console.log([a[propName].id,b[propName].id],'val test')
 
         // If pitcher id properties are not equal,
         // objects are not equivalent
